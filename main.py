a74415e6cf1f42e073fdbd9fe011f07314f6438c
Misola <misolakhemmy@gmaiil.com>
2025-05-10 13:58:20 +0100

    Add Task 2: User Input & Conditional Statements

--- main.py
+++ main.py
@@ -19,3 +19,20 @@ print("Value:", is_student, "| Type:", type(is_student))  # Output: Value: False
 # List
 favorite_colors = ["blue", "green", "red"]
 print("Value:", favorite_colors, "| Type:", type(favorite_colors))  # Output: Value: ['blue', 'green', 'red'] | Type: <class 'list'>
+
+# Task 2: User Input & Conditional Statements
+
+# Ask the user to input their age
+age_input = input("Enter your age: ")
+
+# Convert input to integer (always needed for numerical comparison)
+try:
+    age = int(age_input)
+
+    # Conditional check
+    if age >= 18:
+        print("You are eligible to vote.")
+    else:
+        print("You are not eligible to vote.")
+except ValueError:
+    print("Invalid input. Please enter a numeric value.")
