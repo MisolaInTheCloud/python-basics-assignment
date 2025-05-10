1ef3afe1a88e881dba8ed3badd8ad43266b2e422
Misola <misolakhemmy@gmaiil.com>
2025-05-10 06:48:36 +0100

    Completed Task 2: User Input & Conditional Statements

---
+++ main.py
@@ -0,0 +1,21 @@
+# Task 1: Variables & Data Types
+
+# String
+name = "Misola"
+print("Value:", name, "| Type:", type(name))  # Output: Value: Misola | Type: <class 'str'>
+
+# Integer
+age = 23
+print("Value:", age, "| Type:", type(age))  # Output: Value: 23 | Type: <class 'int'>
+
+# Float
+height = 5.7
+print("Value:", height, "| Type:", type(height))  # Output: Value: 5.7 | Type: <class 'float'>
+
+# Boolean
+is_student = False
+print("Value:", is_student, "| Type:", type(is_student))  # Output: Value: False | Type: <class 'bool'>
+
+# List
+favorite_colors = ["blue", "green", "red"]
+print("Value:", favorite_colors, "| Type:", type(favorite_colors))  # Output: Value: ['blue', 'green', 'red'] | Type: <class 'list'>
